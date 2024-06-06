6b61157d2b6ded07303b9800d4093e979ba32399
Zain Ul Abid <bajwazain121@outlook.com>
2024-06-06 00:24:36 -0400

    installing the dark theme switcher

--- js/script.js
+++ js/script.js
@@ -66,3 +66,24 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleModeButton = document.getElementById('toggleMode');
+    // const toggleModeButton = $('#toggleMode');
+    function applyTheme(theme) {
+        document.body.classList.toggle('dark-mode', theme === 'dark');
+    }
+
+    function toggleTheme() {
+        const currentTheme = localStorage.getItem('theme') || 'light';
+        const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+        localStorage.setItem('theme', newTheme);
+        applyTheme(newTheme);
+    }
+
+    toggleModeButton.addEventListener('click', toggleTheme);
+
+    // Apply the saved theme on load
+    const savedTheme = localStorage.getItem('theme') || 'light';
+    applyTheme(savedTheme);
+});
